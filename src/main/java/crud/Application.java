f12c2d06df173da641ded4cae799f13e8e52169d
Matti Tahvonen <matti@vaadin.com>
2016-01-21 09:30:17 +0200

    Updating versions and using ServletComponent scan from latest SpringBoot

--- src/main/java/crud/Application.java
+++ src/main/java/crud/Application.java
@@ -2,33 +2,17 @@ package crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+// Enable additional servlet filters for wscdn and cloud hosted fontawesome
+@ServletComponentScan({"com.vaadin.wscdn", "org.peimari.dawn"})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    /*
-     * If this don't compile, issue "mvn install" once. If it still don't 
-     * compile you are most likely using an odd IDE called Eclipse, which 
-     * don't really get maven projects, so you need to configure the 
-     * generated sources from target/generated-sources/vwscdn to be 
-     * included on your classpath: right click on project -> Properties -> 
-     * Java build path -> Source tab -> Add folder.
-     */
-    @Bean
-    public com.vaadin.wscdn.WidgetSet viritinCdnInitializer() {
-        return new com.vaadin.wscdn.WidgetSet();
-    }
-
-    @Bean
-    public org.peimari.dawn.CdnFonts cdnFonts() {
-        return new org.peimari.dawn.CdnFonts();
-    }
-
+    
 }
