546adfec40479ff3f7f3e2ce8e0802ea42ee7f70
Matti Tahvonen <matti@vaadin.com>
2016-07-08 14:33:40 +0300

    Updated the example to use 7.7.0.alpha3

--- src/main/java/crud/Application.java
+++ src/main/java/crud/Application.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-// Enable additional servlet filters for wscdn and cloud hosted fontawesome
-@ServletComponentScan({"com.vaadin.wscdn", "org.peimari.dawn"})
+// Enable additional servlet filter for cloud hosted fontawesome
+@ServletComponentScan("org.peimari.dawn")
 public class Application {
 
     public static void main(String[] args) {
