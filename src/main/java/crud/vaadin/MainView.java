7c109d43a4555cd03bdf5779e53bb0a5d07a283d
Matti Tahvonen <matti@vaadin.com>
2020-07-12 23:43:42 +0300

    Added sort portion to the test script

--- src/main/java/crud/vaadin/MainView.java
+++ src/main/java/crud/vaadin/MainView.java
@@ -28,20 +28,18 @@ import org.vaadin.spring.events.annotation.EventBusListenerMethod;
 @Route
 public class MainView extends VerticalLayout {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	PersonRepository repo;
+    PersonRepository repo;
 
+    public MainView(PersonRepository r) {
+        this.repo = r;
 
-	public MainView(PersonRepository r) {
-		this.repo = r;
-
-                // TODO
-                
-                // 1. List all persons from the repository in a Grid
-                // 2. Make the data binding lazy loaded
-                // 3. Configure the data binding so that the initial estimate of the person count is 1000
-                
-	}
+        // TODO
+        // 1. List all persons from the repository in a Grid
+        // 2. Make the data binding lazy loaded
+        // 3. Configure the data binding so that the initial estimate of the person count is 1000
+        // 4. Make the name column sortable, others non-sortable
+    }
 
 }
